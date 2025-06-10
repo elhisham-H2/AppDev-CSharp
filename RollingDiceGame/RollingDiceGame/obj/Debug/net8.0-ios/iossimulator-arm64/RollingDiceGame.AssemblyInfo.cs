815fd9c7f9f11964b1db98be662fd2082f814a5b
elhisham <hisham.el.shiraida@gmail.com>
2025-06-09 23:27:23 -0400

    Small updates

--- RollingDiceGame/RollingDiceGame/obj/Debug/net8.0-ios/iossimulator-arm64/RollingDiceGame.AssemblyInfo.cs
+++ RollingDiceGame/RollingDiceGame/obj/Debug/net8.0-ios/iossimulator-arm64/RollingDiceGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RollingDiceGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9023422d8246967d37235e244e8d710b1c668060")]
 [assembly: System.Reflection.AssemblyProductAttribute("RollingDiceGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RollingDiceGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
